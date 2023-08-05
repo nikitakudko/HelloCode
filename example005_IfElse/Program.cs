1f5c97141454e28f73a9de145f6638db0c3ae02b
nikitakudko <nikitakudko@gmail.com>
2023-08-05 20:15:27 +0300

    add IfElse

---
+++ example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hurray, It's Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
