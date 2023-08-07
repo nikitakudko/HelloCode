754b550d8beaa3008a1d5be2d9353718b177d5a1
nikitakudko <nikitakudko@gmail.com>
2023-08-07 22:08:02 +0300

    добавил какую-то дичь

---
+++ example011_ArrayLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿void FILLArray(int[] collection)
+{
+    int legth = collection.legth;
+    int index = 0;
+    while(index < legth)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+int[] array = new int[10];
