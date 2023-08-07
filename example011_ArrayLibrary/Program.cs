52151f76c6e2744a7ebdb868e3e4e5bf48e1c3b9
nikitakudko <nikitakudko@gmail.com>
2023-08-07 22:33:10 +0300

    Закончил лекцию 2

--- example011_ArrayLibrary/Program.cs
+++ example011_ArrayLibrary/Program.cs
@@ -1,12 +1,50 @@
-﻿void FILLArray(int[] collection)
+﻿void FillArray(int[] collection)
 {
-    int legth = collection.legth;
+    int length = collection.Length;
     int index = 0;
-    while(index < legth)
+    while(index < length)
     {
         collection[index] = new Random().Next(1, 10);
         index++;
     }
 }
 
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Index0f(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Index0f(array, 4);
+Console.WriteLine(pos);
