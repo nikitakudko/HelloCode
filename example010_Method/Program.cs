668857b8a2481b99a239b4532df2fe2854917ce1
nikitakudko <nikitakudko@gmail.com>
2023-08-07 21:42:08 +0300

    добавил example010

---
+++ example010_Method/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 12, 32, 38, 4, 35, 76, 71, 38};
+
+int n = array.Length;
+int find = 38;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
